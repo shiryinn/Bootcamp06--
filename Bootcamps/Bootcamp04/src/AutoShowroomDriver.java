a4f4dd2b74c20386f03ac477037892256fe59117
shiryinlim <slim0098@student.monash.edu>
2021-03-31 03:38:44 +0800

    bootcamp4

--- Bootcamps/Bootcamp04/src/AutoShowroomDriver.java
+++ Bootcamps/Bootcamp04/src/AutoShowroomDriver.java
@@ -4,5 +4,7 @@ public class AutoShowroomDriver {
     public static void main(String[] args) throws ParseException {
         AutoShowroom autoShowroom = new AutoShowroom();
         autoShowroom.printStatus(true);
+
+
     }
 }
